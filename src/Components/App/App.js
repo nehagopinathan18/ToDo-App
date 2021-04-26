2fca27c7c7eb4787f53e4b9aa73bc8b27bd5cb4b
Neha Gopinathan <nehagopinathan18@gmail.com>
2021-04-26 06:40:59 +0530

    changed

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './../Header/Header';
-import firebase from 'firebase/app';
-// import { auth } from "./firebase";
+
 
 class App extends React.Component{
   constructor() {
